c870ca7496ce563b74f462c6dc81089e5505ea4b
fardinnb864457h <narutoshippuden6654@gmail.com>
2026-02-09 07:08:12 +0600

    Delete Player.js

--- Player.js
+++
@@ -1,6 +0,0 @@
-<script src="https://mdfardin857875557.github.io/nice-JS-PLAY/playerjs.js" type="text/javascript"></script>
-       
-<div id="player">
-</div>
-  </main>
-<script>var player = new Playerjs({id:"player", file:'https://byphdgllyk.gpcdn.net/hls/deeptotv/0_1/index.m3u8', poster:""});</script>
